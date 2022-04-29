35e13c27751a06a9975053f623f9f6e1d231c527
BeaAnt <57292753+BeaAnt@users.noreply.github.com>
2022-04-29 13:43:53 +0200

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,7 +29,7 @@ import { AlertComponent } from './_components';
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
 
         
     ],
